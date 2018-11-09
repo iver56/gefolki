915b89dcd07fee323c1dad7be391b0b5a03d80c5
iver56 <ij@aiascience.com>
2018-11-09 13:47:56 +0100

    Save norm figure in simple_demo

--- gefolki/simple_demo.py
+++ gefolki/simple_demo.py
@@ -15,22 +15,26 @@ def run_simple_demo():
     img2[60:310, 60:310] = 1.0
 
     img1_pil = Image.fromarray((img1 * 255).astype(np.uint8))
-    img1_pil.save(DATA_DIR / 'img1.png')
+    img1_pil.save(DATA_DIR / "img1.png")
 
     img2_pil = Image.fromarray((img2 * 255).astype(np.uint8))
-    img2_pil.save(DATA_DIR / 'img2.png')
+    img2_pil.save(DATA_DIR / "img2.png")
 
     u, v = EFolki(img1, img2, iteration=2, radius=[32, 24, 16, 8], rank=4, levels=5)
-    N = np.sqrt(u ** 2 + v ** 2)
+
+    optical_flow_norm = np.sqrt(u ** 2 + v ** 2)
+
     pl.figure()
-    pl.imshow(N)
+    pl.imshow(optical_flow_norm)
     pl.title("Norm of optical flow")
     pl.colorbar()
+    pl.savefig(DATA_DIR / "optical_flow_norm.png")
 
     img2_resampled = wrapData(img2, u, v)
 
     img2_resampled_pil = Image.fromarray((img2_resampled * 255).astype(np.uint8))
-    img2_resampled_pil.save(DATA_DIR / 'img2_resampled.png')
+    img2_resampled_pil.save(DATA_DIR / "img2_resampled.png")
+
 
 
 if __name__ == "__main__":
